bf99b8c52d833077060f8f4828b28ed3ffca9a31
skoriop <karthikp.ivy@gmail.com>
2023-09-28 08:10:40 +0530

    add skeleton ui

--- routes/index.js
+++ routes/index.js
@@ -7,27 +7,41 @@ var path = require("path");
 var fs = require("fs");
 let appRoot = require("app-root-path");
 
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   res.render("index");
 });
-router.get("/team", function(req, res, next) {
+
+router.get("/team", function (req, res, next) {
   res.render("team", {
     users: users[config.siteMode],
     column: 3,
     totalModules: 17
   });
 });
-router.get("/type", function(req, res, next) {
+
+router.get("/test", function (req, res, next) {
+  res.render("test", {
+    dashboard: {
+      type: "Administrator"
+    },
+    user: {
+      name: "Karthik"
+    },
+    portals: [],
+  });
+});
+
+router.get("/type", function (req, res, next) {
   res.render("type");
 });
 
-router.get("/fd-thesis", function(req, res) {
-  fs.readdir(appRoot.path + "/public/AUGSD/fd-thesis", function(err, files) {
+router.get("/fd-thesis", function (req, res) {
+  fs.readdir(appRoot.path + "/public/AUGSD/fd-thesis", function (err, files) {
     res.render("fd-thesis", { forms: files });
   });
 });
-router.get("/registration", function(req, res) {
-  fs.readdir(appRoot.path + "/public/AUGSD/registration", function(err, files) {
+router.get("/registration", function (req, res) {
+  fs.readdir(appRoot.path + "/public/AUGSD/registration", function (err, files) {
     res.render("registration", { forms: files });
   });
 });
